c95d42be34d38bf509f0946944630fceb9afdef3
Pushkar Deshmukh <pushkard@sminq.com>
2017-11-03 18:22:39 +0530

    login functionality

--- platforms/android/src/main/assets/app/views/login/login.js
+++ platforms/android/src/main/assets/app/views/login/login.js
@@ -1,26 +1,32 @@
 var frameModule = require("ui/frame");
+var UserViewModel = require("../../shared/view-models/user-view-model");
+var dialogsModule = require("ui/dialogs");
 
+var user = new UserViewModel({
+    email: "username@domain.com",
+    password: "password"
+});
 var page;
 var email;
 
-var observableModule = require("data/observable");
-
-var user = new observableModule.fromObject({
-    email: "user@domain.com",
-    password: "password"
-});
-
 exports.loaded = function (args) {
-    console.log("hello");
     page = args.object;
-    console.log(page)
     page.bindingContext = user;
 };
 
 exports.signIn = function () {
-    alert("Signing in");
-    email = page.getViewById("email");
-    console.log(email.text);
+    user.login()
+        .catch(function (error) {
+            console.log(error);
+            dialogsModule.alert({
+                message: "Unfortunately we could not find your account.",
+                okButtonText: "OK"
+            });
+            return Promise.reject();
+        })
+        .then(function () {
+            frameModule.topmost().navigate("views/list/list");
+        });
 };
 
 exports.register = function () {
