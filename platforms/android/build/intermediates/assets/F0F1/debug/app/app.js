c1561b8ae996544d4484cec1be44d803fa1a541e
Pushkar Deshmukh <pushkard@sminq.com>
2017-11-07 10:37:33 +0530

    Showed remote config on login screen

--- platforms/android/build/intermediates/assets/F0F1/debug/app/app.js
+++ platforms/android/build/intermediates/assets/F0F1/debug/app/app.js
@@ -12,6 +12,23 @@ firebase.init({
     function (error) {
         console.log("firebase.init error: " + error);
     }
-    );
-    
-applicationModule.start({ moduleName: "views/login/login" });
+);
+
+// firebase.getRemoteConfig({
+//     developerMode: false, // play with this boolean to get more frequent updates during development
+//     cacheExpirationSeconds: 600, // 10 minutes, default is 12 hours.. set to a lower value during dev
+//     properties: [{
+//             key: "is_enabled",
+//             default: 0
+//         }
+//     ]
+// }).then(
+//     function (result) {
+//         console.log("Remote Config last fetched at " + result.lastFetch);
+//         console.log("Remote Config: " + JSON.stringify(result.properties));
+//     }
+// );
+
+applicationModule.start({
+    moduleName: "views/login/login"
+});
--- platforms/android/build/intermediates/assets/F0F1/debug/app/views/login/login.js
+++ platforms/android/build/intermediates/assets/F0F1/debug/app/views/login/login.js
@@ -1,17 +1,38 @@
 var frameModule = require("ui/frame");
 var UserViewModel = require("../../shared/view-models/user-view-model");
 var dialogsModule = require("ui/dialogs");
+var firebase = require("nativescript-plugin-firebase");
 
 var user = new UserViewModel({
     email: "testing@gmail.comm",
-    password: "test"
+    password: "test",
+    isEnabledFirebaseFlag: 'asd'
 });
 var page;
 var email;
 
+
 exports.loaded = function (args) {
     page = args.object;
     page.bindingContext = user;
+
+    firebase.getRemoteConfig({
+        developerMode: false, // play with this boolean to get more frequent updates during development
+        cacheExpirationSeconds: 600, // 10 minutes, default is 12 hours.. set to a lower value during dev
+        properties: [{
+            key: "is_enabled",
+            default: 0
+        }
+        ]
+    }).then(
+        function (result) {
+            console.log('===================')
+            console.dir(result)
+            console.log("Remote Config last fetched at " + result.lastFetch);
+            console.log("Remote Config: " + JSON.stringify(result.properties));
+            user.isEnabledFirebaseFlag = result.properties.is_enabled;
+        }
+        );
 };
 
 exports.signIn = function () {
