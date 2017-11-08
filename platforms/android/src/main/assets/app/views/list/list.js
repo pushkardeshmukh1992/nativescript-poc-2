257888ca7c359c8d365065f3d7a940c720c673ad
Pushkar Deshmukh <pushkard@sminq.com>
2017-11-08 13:21:10 +0530

    Conditionally showed two different CTAs based on remote config flag. Added custom events for same

---
+++ platforms/android/src/main/assets/app/views/list/list.js
@@ -0,0 +1,81 @@
+var dialogsModule = require("ui/dialogs");
+var GroceryListViewModel = require("../../shared/view-models/grocery-list-view-model");
+var swipeDelete = require("../../shared/utils/ios-swipe-delete");
+
+var observableModule = require("data/observable")
+var ObservableArray = require("data/observable-array").ObservableArray;
+var page;
+
+var groceryList = new GroceryListViewModel([]);
+var pageData = new observableModule.fromObject({
+    groceryList: groceryList,
+    grocery: ""
+});
+
+var socialShare = require("nativescript-social-share");
+
+exports.loaded = function (args) {
+    page = args.object;
+
+    if (page.ios) {
+        var listView = page.getViewById("groceryList");
+        swipeDelete.enable(listView, function (index) {
+            groceryList.delete(index);
+        });
+    }
+    
+    var listView = page.getViewById("groceryList");
+    page.bindingContext = pageData;
+
+    groceryList.empty();
+    pageData.set("isLoading", true);
+    groceryList.load().then(function () {
+        pageData.set("isLoading", false);
+        listView.animate({
+            opacity: 1,
+            duration: 1000
+        });
+    });
+};
+
+exports.add = function () {
+    // Check for empty submissions
+    if (pageData.get("grocery").trim() === "") {
+        dialogsModule.alert({
+            message: "Enter a grocery item",
+            okButtonText: "OK"
+        });
+        return;
+    }
+    // Dismiss the keyboard
+    page.getViewById("grocery").dismissSoftInput();
+    groceryList.add(pageData.get("grocery"))
+        .catch(function () {
+            dialogsModule.alert({
+                message: "An error occurred while adding an item to your list.",
+                okButtonText: "OK"
+            });
+        });
+
+    // Empty the input field
+    pageData.set("grocery", "");
+};
+
+exports.share = function () {
+    var list = [];
+    for (var i = 0, size = groceryList.length; i < size; i++) {
+        list.push(groceryList.getItem(i).name);
+    }
+    var listString = list.join(", ").trim();
+    socialShare.shareText(listString);
+};
+
+exports.delete = function (args) {
+    console.log('in delete method');
+    var item = args.view.bindingContext;
+    var index = groceryList.indexOf(item);
+    pageData.set("isLoading", true);
+    groceryList.delete(index).then(function () {
+        pageData.set("isLoading", false);
+    });
+};
